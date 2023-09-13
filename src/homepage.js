917228b20ad40066e7358f4e530f23998915e240
amme987 <emmagination97@gmail.com>
2023-09-13 11:38:07 -0400

    Replace homepage image with new background image

--- src/homepage.js
+++ src/homepage.js
@@ -1,21 +1,17 @@
 import Food from "./homepage.jpg";
 
 const initialLoad = () => {
-  const h1 = document.createElement("h1");
-  document.body.appendChild(h1).textContent = "The Hungry Fork";
+  const content = document.getElementById("content");
 
-  const aside = document.createElement("aside");
-  document.body.appendChild(aside).textContent = "Fast, fresh, and forkable";
-
-  const div = document.createElement("div");
-  document.body.appendChild(div).setAttribute("class", "homepage");
-
+  const main = document.createElement("main");
+  content.appendChild(main);
   const img = new Image();
   img.src = Food;
-  div.appendChild(img);
-
+  //   main.appendChild(img);
+  const aside = document.createElement("aside");
+  main.appendChild(aside).textContent = "Fast, fresh, and forkable";
   const p = document.createElement("p");
-  div.appendChild(p).textContent =
+  main.appendChild(p).textContent =
     "The Hungry Fork is a convenient and affordable restaurant that offers a variety of dishes that can be eaten with a fork. Whether you want a salad, a pasta, a stir-fry, or a pie, you will find it at The Hungry Fork. Our food is made with fresh and quality ingredients and prepared in minutes. No matter how busy you are, you can always grab a delicious and satisfying meal at The Hungry Fork.";
 };
 export default initialLoad;
--- src/index.js
+++ src/index.js
@@ -2,4 +2,15 @@ import "./style.css";
 // import Icon from "./fork.jpg";
 import initialLoad from "./homepage";
 
+const displayTabs = (() => {
+  const nav = document.createElement("nav");
+  content.appendChild(nav);
+  const homepage = document.createElement("a");
+  nav.appendChild(homepage).textContent = "The Hungry Fork";
+  const menu = document.createElement("a");
+  nav.appendChild(menu).textContent = "Menu";
+  const contact = document.createElement("a");
+  nav.appendChild(contact).textContent = "Contact";
+})();
+
 initialLoad();
